11a81297ab8130f969a19cd9051c9a4a947afdf0
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-24 12:33:40 +0530

    Create broadcast.py

---
+++ JARVIS/modules/broadcast.py
@@ -0,0 +1,24 @@
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from config import BOT_TOKEN
+# Define your Telegram bot token here
+TOKEN = BOT_TOKEN
+
+def broadcast_message(update: Update, context: CallbackContext) -> None:
+    message = ' '.join(context.args)
+    for update in context.bot.get_updates():
+        chat_id = update.message.chat_id
+        context.bot.send_message(chat_id=chat_id, text=message)
+
+def main() -> None:
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('broadcast', broadcast_message))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
