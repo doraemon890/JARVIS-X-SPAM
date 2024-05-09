e444e458dc4d357ee6973b687fe4df02e243fe04
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-09 11:49:12 +0530

    Update start.py

--- JARVIS/modules/start.py
+++ JARVIS/modules/start.py
@@ -8,7 +8,7 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_V_SUPPORT"),
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_V_SUPPORT/5"),
         Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/Dora_Hub")
     ],
     [
