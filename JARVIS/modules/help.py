3688d14481faea3bc5992183205ee81d081502ec
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-08 19:38:50 +0530

    Update help.py

--- JARVIS/modules/help.py
+++ JARVIS/modules/help.py
@@ -1,203 +1,108 @@
 from telethon import events, Button
+from config import (
+    X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
+)
 
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
-
-
-HELP_STRING = f"★ ᴊᴀʀᴠɪs ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @JARVIS_V2**"
+# Help Menu Constants
+HELP_STRING = (
+    "**𖤍 ᴊᴀʀᴠɪs sᴘᴀᴍ ʜᴇʟᴘ ᴍᴇɴᴜ 𖤍**\n\n"
+    "» ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ\n"
+    "» **ᴅᴇᴠᴇʟᴏᴘᴇʀ**: @JARVIS_V2"
+)
 
 HELP_BUTTON = [
-    [
-      Button.inline("• ꜱᴘᴀᴍ •", data="spam"),
-      Button.inline("• ʀᴀɪᴅ •", data="raid")
-    ],
-    [
-      Button.inline("• ᴇxᴛʀᴀ •", data="extra")
-    ],
-    [
-      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_V_SUPPORT"),
-      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/Dora_Hub")
-    ]
-  ]
+    [Button.inline("• ꜱᴘᴀᴍ •", data="spam"), Button.inline("• ʀᴀɪᴅ •", data="raid")],
+    [Button.inline("• ᴇxᴛʀᴀ •", data="extra")],
+    [Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_V_SUPPORT"), Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/Dora_Hub")]
+]
 
+extra_msg = (
+    "**» ᴇxᴛʀᴀ ᴄᴏᴍᴍᴀɴᴅꜱ:**\n\n"
+    "𝗨𝘀𝗲𝗿𝗕𝗼𝘁: **ᴜꜱᴇʀʙᴏᴛ ᴄᴍᴅꜱ**\n"
+    f"  1) {hl}ping\n"
+    f"  2) {hl}reboot\n"
+    f"  3) {hl}sudo <reply to user>  --> Owner Cmd\n"
+    f"  4) {hl}logs --> Owner Cmd\n\n"
+    "𝗘𝗰𝗵𝗼: **ᴛᴏ ᴀᴄᴛɪᴠᴇ ᴇᴄʜᴏ ᴏɴ ᴀɴʏ ᴜꜱᴇʀ**\n"
+    f"  1) {hl}echo <reply to user>\n"
+    f"  2) {hl}rmecho <reply to user>\n\n"
+    "𝗟𝗲𝗮𝘃𝗲: **ᴛᴏ ʟᴇᴀᴠᴇ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ**\n"
+    f"  1) {hl}leave <group/chat id>\n"
+    f"  2) {hl}leave : Type in the Group bot will auto leave that group\n\n"
+    "**© @JARVIS_V2**"
+)
 
-@X1.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X2.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X3.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X4.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X5.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X6.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X7.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X8.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X9.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X10.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-async def help(event):
+raid_msg = (
+    "**» ʀᴀɪᴅ ᴄᴏᴍᴍᴀɴᴅꜱ:**\n\n"
+    "𝗥𝗮𝗶𝗱: **ᴀᴄᴛɪᴠᴀᴛᴇꜱ ʀᴀɪᴅ ᴏɴ ᴀɴʏ ɪɴᴅɪᴠɪᴅᴜᴀʟ ᴜꜱᴇʀ ꜰᴏʀ ɢɪᴠᴇɴ ʀᴀɴɢᴇ.**\n"
+    f"  1) {hl}raid <count> <username>\n"
+    f"  2) {hl}raid <count> <reply to user>\n\n"
+    "𝗥𝗲𝗽𝗹𝘆𝗥𝗮𝗶𝗱: **ᴀᴄᴛɪᴠᴀᴛᴇꜱ ʀᴇᴘʟʏ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**\n"
+    f"  1) {hl}rraid <replying to user>\n"
+    f"  2) {hl}rraid <username>\n\n"
+    "𝗗𝗥𝗲𝗽𝗹𝘆𝗥𝗮𝗶𝗱: **ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇꜱ ʀᴇᴘʟʏ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**\n"
+    f"  1) {hl}drraid <replying to user>\n"
+    f"  2) {hl}drraid <username>\n\n"
+    "𝐌𝐑𝐚𝐢𝐝: **ʟᴏᴠᴇ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**\n"
+    f"  1) {hl}mraid <count> <username>\n"
+    f"  2) {hl}mraid <count> <reply to user>\n\n"
+    "𝐒𝐑𝐚𝐢𝐝: **ꜱʜᴀʏᴀʀɪ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**\n"
+    f"  1) {hl}sraid <count> <username>\n"
+    f"  2) {hl}sraid <count> <reply to user>\n\n"
+    "𝐐𝐑𝐚𝐢𝐝: **ᴀʙᴄᴅ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**\n"
+    f"  1) {hl}qraid <count> <username>\n"
+    f"  2) {hl}qraid <count> <reply to user>\n\n"
+    "**© @JARVIS_V2**"
+)
+
+spam_msg = (
+    "**» ꜱᴘᴀᴍ ᴄᴏᴍᴍᴀɴᴅꜱ:**\n\n"
+    "𝗦𝗽𝗮𝗺: **ꜱᴘᴀᴍꜱ ᴀ ᴍᴇꜱꜱᴀɢᴇ.**\n"
+    f"  1) {hl}spam <count> <message to spam> (you can reply any message if you want bot to reply that message and do spamming)\n"
+    f"  2) {hl}spam <count> <replying any message>\n\n"
+    "𝗣𝗼𝗿𝗻𝗦𝗽𝗮𝗺: **ᴘᴏʀɴᴏɢʀᴀᴘʜʏ ꜱᴘᴀᴍ.**\n"
+    f"  1) {hl}pspam <count>\n\n"
+    "𝗛𝗮𝗻𝗴: **ꜱᴘᴀᴍꜱ ʜᴀɴɢɪɴɢ ᴍᴇꜱꜱᴀɢᴇ ꜰᴏʀ ɢɪᴠᴇɴ ᴄᴏᴜɴᴛᴇʀ.**\n"
+    f"  1) {hl}hang <counter>\n\n"
+    "**© @JARVIS_V2**"
+)
+
+# Event Handler Functions
+async def show_help(event):
     if event.sender_id in SUDO_USERS:
         try:
-          await event.client.send_file(event.chat_id,
-              "https://telegra.ph/file/41b903c834a8af32e2281.jpg",
-              caption=HELP_STRING,
-              buttons=HELP_BUTTON
-              )
+            await event.client.send_file(
+                event.chat_id,
+                "https://telegra.ph/file/41b903c834a8af32e2281.jpg",
+                caption=HELP_STRING,
+                buttons=HELP_BUTTON
+            )
         except Exception as e:
             await event.client.send_message(event.chat_id, f"An Exception Occured!\n\n**ERROR:** {str(e)}")
 
-
-extra_msg = f"""
-**» ᴇxᴛʀᴀ ᴄᴏᴍᴍᴀɴᴅꜱ:**
-
-𝗨𝘀𝗲𝗿𝗕𝗼𝘁: **ᴜꜱᴇʀʙᴏᴛ ᴄᴍᴅꜱ**
-  1) {hl}ping 
-  2) {hl}reboot
-  3) {hl}sudo <reply to user>  --> Owner Cmd
-  4) {hl}logs --> Owner Cmd
-
-𝗘𝗰𝗵𝗼: **ᴛᴏ ᴀᴄᴛɪᴠᴇ ᴇᴄʜᴏ ᴏɴ ᴀɴʏ ᴜꜱᴇʀ**
-  1) {hl}echo <reply to user>
-  2) {hl}rmecho <reply to user>
-
-𝗟𝗲𝗮𝘃𝗲: **ᴛᴏ ʟᴇᴀᴠᴇ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ**
-  1) {hl}leave <group/chat id>
-  2) {hl}leave : Type in the Group bot will auto leave that group
-
-
-**© @JARVIS_V2**
-"""
-
-                 
-raid_msg = f"""
-**» ʀᴀɪᴅ ᴄᴏᴍᴍᴀɴᴅꜱ:**
-
-𝗥𝗮𝗶𝗱: **ᴀᴄᴛɪᴠᴀᴛᴇꜱ ʀᴀɪᴅ ᴏɴ ᴀɴʏ ɪɴᴅɪᴠɪᴅᴜᴀʟ ᴜꜱᴇʀ ꜰᴏʀ ɢɪᴠᴇɴ ʀᴀɴɢᴇ.**
-  1) {hl}raid <count> <username>
-  2) {hl}raid <count> <reply to user>
-
-𝗥𝗲𝗽𝗹𝘆𝗥𝗮𝗶𝗱: **ᴀᴄᴛɪᴠᴀᴛᴇꜱ ʀᴇᴘʟʏ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**
-  1) {hl}rraid <replying to user>
-  2) {hl}rraid <username>
-
-𝗗𝗥𝗲𝗽𝗹𝘆𝗥𝗮𝗶𝗱: **ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇꜱ ʀᴇᴘʟʏ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**
-  1) {hl}drraid <replying to user>
-  2) {hl}drraid <username>
-
-𝐌𝐑𝐚𝐢𝐝: **ʟᴏᴠᴇ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**
-  1) {hl}mraid <count> <username>
-  2) {hl}mraid <count> <reply to user>
-
-𝐒𝐑𝐚𝐢𝐝: **ꜱʜᴀʏᴀʀɪ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**
-  1) {hl}sraid <count> <username>
-  2) {hl}sraid <count> <reply to user>
-
-𝐐𝐑𝐚𝐢𝐝: **ᴀʙᴄᴅ ʀᴀɪᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ.**
-  1) {hl}qraid <count> <username>
-  2) {hl}qraid <count> <reply to user>
-
-
-**© @JARVIS_V2**
-"""
-
-spam_msg = f"""
-**» ꜱᴘᴀᴍ ᴄᴏᴍᴍᴀɴᴅꜱ:**
-
-𝗦𝗽𝗮𝗺: **ꜱᴘᴀᴍꜱ ᴀ ᴍᴇꜱꜱᴀɢᴇ.**
-  1) {hl}spam <count> <message to spam> (you can reply any message if you want bot to reply that message and do spamming)
-  2) {hl}spam <count> <replying any message>
-
-𝗣𝗼𝗿𝗻𝗦𝗽𝗮𝗺: **ᴘᴏʀᴍᴏɢʀᴀᴘʜʏ ꜱᴘᴀᴍ.**
-  1) {hl}pspam <count>
-
-𝗛𝗮𝗻𝗴: **ꜱᴘᴀᴍꜱ ʜᴀɴɢɪɴɢ ᴍᴇꜱꜱᴀɢᴇ ꜰᴏʀ ɢɪᴠᴇɴ ᴄᴏᴜɴᴛᴇʀ.**
-  1) {hl}hang <counter>
-
-
-** © @JARVIS_V2**
-"""                     
-           
-           
-@X1.on(events.CallbackQuery(pattern=r"help_back"))
-@X2.on(events.CallbackQuery(pattern=r"help_back"))
-@X3.on(events.CallbackQuery(pattern=r"help_back"))
-@X4.on(events.CallbackQuery(pattern=r"help_back"))
-@X5.on(events.CallbackQuery(pattern=r"help_back"))
-@X6.on(events.CallbackQuery(pattern=r"help_back"))
-@X7.on(events.CallbackQuery(pattern=r"help_back"))
-@X8.on(events.CallbackQuery(pattern=r"help_back"))
-@X9.on(events.CallbackQuery(pattern=r"help_back"))
-@X10.on(events.CallbackQuery(pattern=r"help_back"))
 async def helpback(event):
-    if event.query.user_id in SUDO_USERS:    
-        await event.edit(
-            HELP_STRING,
-            buttons=[
-              [
-                Button.inline("• ꜱᴘᴀᴍ •", data="spam"),
-                Button.inline("• ʀᴀɪᴅ •", data="raid")
-              ],
-              [
-                Button.inline("• ᴇxᴛʀᴀ •", data="extra")
-              ],
-              [
-                Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_V_SUPPORT"),
-                Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/Dora_Hub")
-              ]
-            ]
-          )
+    if event.query.user_id in SUDO_USERS:
+        await event.edit(HELP_STRING, buttons=HELP_BUTTON)
     else:
-        await event.answer("ᴘᴀʜʟᴇ ᴊᴀʀᴠɪs ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @JARVIS_V2", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ ᴊᴀʀᴠɪs ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ☎️ @JARVIS_V2", cache_time=0, alert=True)
 
-
-@X1.on(events.CallbackQuery(pattern=r"spam"))
-@X2.on(events.CallbackQuery(pattern=r"spam"))
-@X3.on(events.CallbackQuery(pattern=r"spam"))
-@X4.on(events.CallbackQuery(pattern=r"spam"))
-@X5.on(events.CallbackQuery(pattern=r"spam"))
-@X6.on(events.CallbackQuery(pattern=r"spam"))
-@X7.on(events.CallbackQuery(pattern=r"spam"))
-@X8.on(events.CallbackQuery(pattern=r"spam"))
-@X9.on(events.CallbackQuery(pattern=r"spam"))
-@X10.on(events.CallbackQuery(pattern=r"spam"))
 async def help_spam(event):
-    if event.query.user_id in SUDO_USERS:    
-        await event.edit(spam_msg,
-              buttons=[[Button.inline("< Back", data="help_back"),],],
-              ) 
-    else:
-        await event.answer("ᴘᴀʜʟᴇ ᴊᴀʀᴠɪs ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @JARVIS_V2", cache_time=0, alert=True)
+    if event.query.user_id in SUDO_USERS:
+        await event.edit(spam_msg, buttons=[[Button.inline("< Back", data="help_back")]])
 
-
-@X1.on(events.CallbackQuery(pattern=r"raid"))
-@X2.on(events.CallbackQuery(pattern=r"raid"))
-@X3.on(events.CallbackQuery(pattern=r"raid"))
-@X4.on(events.CallbackQuery(pattern=r"raid"))
-@X5.on(events.CallbackQuery(pattern=r"raid"))
-@X6.on(events.CallbackQuery(pattern=r"raid"))
-@X7.on(events.CallbackQuery(pattern=r"raid"))
-@X8.on(events.CallbackQuery(pattern=r"raid"))
-@X9.on(events.CallbackQuery(pattern=r"raid"))
-@X10.on(events.CallbackQuery(pattern=r"raid"))
 async def help_raid(event):
     if event.query.user_id in SUDO_USERS:
-        await event.edit(raid_msg,
-            buttons=[[Button.inline("< Back", data="help_back"),],],
-          )
-    else:
-        await event.answer("ᴘᴀʜʟᴇ ᴊᴀʀᴠɪs ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @JARVIS_V2", cache_time=0, alert=True)
+        await event.edit(raid_msg, buttons=[[Button.inline("< Back", data="help_back")]])
 
-
-@X1.on(events.CallbackQuery(pattern=r"extra"))
-@X2.on(events.CallbackQuery(pattern=r"extra"))
-@X3.on(events.CallbackQuery(pattern=r"extra"))
-@X4.on(events.CallbackQuery(pattern=r"extra"))
-@X5.on(events.CallbackQuery(pattern=r"extra"))
-@X6.on(events.CallbackQuery(pattern=r"extra"))
-@X7.on(events.CallbackQuery(pattern=r"extra"))
-@X8.on(events.CallbackQuery(pattern=r"extra"))
-@X9.on(events.CallbackQuery(pattern=r"extra"))
-@X10.on(events.CallbackQuery(pattern=r"extra"))
 async def help_extra(event):
     if event.query.user_id in SUDO_USERS:
-        await event.edit(extra_msg,
-            buttons=[[Button.inline("< Back", data="help_back"),],],
-            )
-    else:
-        await event.answer("ᴘᴀʜʟᴇ ᴊᴀʀᴠɪs ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @JARVIS_V2", cache_time=0, alert=True)
+        await event.edit(extra_msg, buttons=[[Button.inline("< Back", data="help_back")]])
+
+# Register Event Handlers for Help Command
+for handler in [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10]:
+    handler.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))(show_help)
+    handler.on(events.CallbackQuery(pattern=r"help_back"))(helpback)
+    handler.on(events.CallbackQuery(pattern=r"spam"))(help_spam)
+    handler.on(events.CallbackQuery(pattern=r"raid"))(help_raid)
+    handler.on(events.CallbackQuery(pattern=r"extra"))(help_extra)
 
