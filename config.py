221404997095bf0350058f5dfd3d963cc1759b2a
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-08 19:55:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 
 BOT_TOKEN = getenv("BOT_TOKEN", default=None)
 BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
