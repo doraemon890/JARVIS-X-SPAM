82d54bee01d6851dbcba3158f69a591c48758bd1
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-11-27 12:50:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,14 @@ from os import getenv
 from telethon import TelegramClient
 from JARVIS.data import FRIDAY
 
-# Configure logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-# Environment variables and constants
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 24509589
+API_HASH = "717cf21d94c4934bcbe1eaa1ad86ae75"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
-HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-# Bot tokens
+
 BOT_TOKENS = [
     getenv("BOT_TOKEN", default=None),
     getenv("BOT_TOKEN2", default=None),
@@ -28,13 +24,8 @@ BOT_TOKENS = [
     getenv("BOT_TOKEN10", default=None),
 ]
 
-# Sudo users and owner ID
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", default="6757745933").split()))
-SUDO_USERS.extend(FRIDAY)
 OWNER_ID = int(getenv("OWNER_ID", default="7044783841"))
-SUDO_USERS.append(OWNER_ID)
 
-# Initialize Telegram clients
 clients = []
 for idx, bot_token in enumerate(BOT_TOKENS, start=1):
     if bot_token:
@@ -42,6 +33,5 @@ for idx, bot_token in enumerate(BOT_TOKENS, start=1):
         clients.append(client)
         logging.info(f'Initialized bot X{idx}')
 
-# Assign clients to specific variables for convenience
 X1, X2, X3, X4, X5, X6, X7, X8, X9, X10 = clients[:10]
 
